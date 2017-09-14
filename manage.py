58f041a2b8960bd5838f59ef6e30f53495b174c3
yige <a@wyr.me>
2017-09-14 09:11:20 +0800

    换回pyjwt

--- manage.py
+++ manage.py
@@ -1,22 +1,20 @@
 #coding=utf-8
 import paramiko
 import requests
-from flask import Flask, jsonify
+import jwt, datetime, time
+from flask import Flask, jsonify, request
 from flask_cors import CORS
-from flask_jwt import JWT, jwt_required, current_identity
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 app = Flask(__name__)
 app.debug = True
-app.config['SECRET_KEY'] = 'super-secret'
 CORS(app)
-
-jwt = JWT(app, authenticate, identity)
 
 engine = create_engine('sqlite:///:memory:', echo=True)
 Base = declarative_base()
+SECRET_KEY = 'd21kljk21ljk2e'
 
 class User(Base):
     __tablename__ = 'user'
@@ -28,7 +26,7 @@ class User(Base):
 
 DBSession = sessionmaker(bind=engine)
 
-timestamp = time.mktime(datetime.datetime.now().timetuple())
+# timestamp = time.mktime(datetime.datetime.now().timetuple())
 
 # #创建SSH对象
 # ssh = paramiko.SSHClient()
@@ -51,6 +49,31 @@ timestamp = time.mktime(datetime.datetime.now().timetuple())
 
 # print(memResult.decode())
 
+# @staticmethod
+def encode_auth_token(user_id, username, login_time):
+    try:
+        payload = {
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=10),
+            'iat': datetime.datetime.utcnow(),
+            'iss': 'ken',
+            'data': {
+                'id': user_id,
+                'username': username,
+                'login_time': login_time
+            }
+        }
+        print(payload)
+        return jwt.encode(
+            payload,
+            SECRET_KEY,
+            algorithm='HS256'
+        )
+    except Exception as e:
+        return str(e)
+
+def valid_login(username, password):
+    return True
+
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     return jsonify({ 'status': 1 })
@@ -59,11 +82,23 @@ def hello_world():
 def login():
     if valid_login(request.form['username'],
         request.form['password']):
-        payload = { 'username': request.form['username'] }
-        return jsonify({ 'status': 1 , 'result': { 'token': jwt.generate_jwt(payload, key, 'PS256', datetime.timedelta(minutes=5))}})
+        token = encode_auth_token('test', request.form['username'], datetime.datetime.utcnow())
+        print(token)
+        print('开始解码')
+        # print(jwt.decode(token, 'SECRET_KEY', leeway=10, algorithms=['HS256']))
+        # return jsonify({ 'status': 1, 'result': { 'token': token}})
+        return 'xxx'
     else:
         return jsonify({ 'status': 403 })
 
 def sendsms():
     response = requests.get('https://httpbin.org/ip')
     print('Your IP is {0}'.format(response.json()['origin']))
+
+@app.errorhandler(404)
+def bad_request(e):
+    return jsonify({ 'status': 404 }), 404
+
+@app.errorhandler(500)
+def bad_request(e):
+    return jsonify({ 'status': 500 }), 500
