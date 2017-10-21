2c96af98740d9b3ea642b0c884c5a088efd52099
yige <a@wyr.me>
2017-10-21 18:02:23 +0800

    update - 2017-10-21 18:02:23

--- app/redis.py
+++ app/redis.py
@@ -2,5 +2,6 @@
 # -*- coding:utf-8 -*-
 import redis
 
-pool = redis.ConnectionPool(host='localhost', port=6379, db=0, max_connections=80)
+# pool = redis.ConnectionPool(host='localhost', port=6379, db=0, max_connections=80)
+pool = redis.ConnectionPool(host='localhost', port=6379, db=0, max_connections=8000)
 redis = redis.Redis(connection_pool=pool)
